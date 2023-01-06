6385a2dd003cf26cd49a11f8a25424c76a87bb02
Paul T <developer.paul.123@gmail.com>
2023-01-06 12:34:08 -0500

    Test updates

--- test/models/cat_trivia_model_test.dart
+++ test/models/cat_trivia_model_test.dart
@@ -1,28 +1,26 @@
 import 'dart:convert';
 
-import 'package:felinefacts/entities/cat_trivia.dart';
-import 'package:felinefacts/models/cat_trivia_model.dart';
+import 'package:felinefacts/data/models/cat_trivia_model.dart';
+import 'package:felinefacts/domain/entities/cat_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../fixtures/fixture_reader.dart';
 
 void main() {
   final model = CatTriviaModel(
-    fact: "During the time of the Spanish Inquisition, Pope Innocent VIII condemned cats as evil and thousands of cats were burned. Unfortunately, the widespread killing of cats led to an explosion of the rat population, which exacerbated the effects of the Black Death.", 
-    factLength: 259
-    );
+      fact:
+          "During the time of the Spanish Inquisition, Pope Innocent VIII condemned cats as evil and thousands of cats were burned. Unfortunately, the widespread killing of cats led to an explosion of the rat population, which exacerbated the effects of the Black Death.",
+      factLength: 259);
   test('Should be a subclass of CatTrivia', () async {
     expect(model, isA<CatTrivia>());
   });
 
   group('fromJson', () {
-    test(
-      'Should return valid model from JSON',
-      () async {
-          final Map<String, dynamic> jsonMap = json.decode(fixture('cat_fact.json'));
-          final result = CatTriviaModel.fromMap(jsonMap);
-          expect(result, model);
-      }
-    );
+    test('Should return valid model from JSON', () async {
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('cat_fact.json'));
+      final result = CatTriviaModel.fromMap(jsonMap);
+      expect(result, model);
+    });
   });
 }
--- test/repositories/get_cat_facts_test.dart
+++ test/repositories/get_cat_facts_test.dart
@@ -1,37 +1,29 @@
 import 'package:dartz/dartz.dart';
-import 'package:felinefacts/entities/cat_trivia.dart';
-import 'package:felinefacts/repositories/cat_trivia_repository.dart';
-import 'package:felinefacts/usecases/get_cat_trivia.dart';
-import 'package:felinefacts/usecases/use_case.dart';
+import 'package:felinefacts/domain/entities/cat_trivia.dart';
+import 'package:felinefacts/domain/repositories/cat_trivia_repository.dart';
+import 'package:felinefacts/domain/usecases/get_cat_trivia.dart';
+import 'package:felinefacts/domain/usecases/use_case.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class MockCatFactsRepository extends Mock implements BaseCatTriviaRepository {
-}
+class MockCatFactsRepository extends Mock implements BaseCatTriviaRepository {}
 
 void main() {
-  GetCatTriviaUseCase useCase;
-  MockCatFactsRepository repository;
-
-  setUp(() {
-    repository = MockCatFactsRepository();
-    useCase = GetCatTriviaUseCase(repository);
-  });
+  MockCatFactsRepository repository = MockCatFactsRepository();
+  GetCatTriviaUseCase useCase = GetCatTriviaUseCase(repository);
 
   final testCatTrivia = CatTrivia(fact: "", factLength: 0);
 
-  test('Should get single trivia fact for cats from repository',
-      () async {
-        // arrange
-        // when `getCatFact()` is called with any return the above test object.
-        when(repository.getCatFact())
-            .thenAnswer((_) async => Right(testCatTrivia));
-        // act
-        final result = await useCase(UseCaseParameters(factLimit: 10, maxLength: 200));
-        // assert
-        expect(result, Right(testCatTrivia));
-        verify(repository.getCatFact());
-        verifyNoMoreInteractions(repository);
-      }
-  );
+  test('Should get single trivia fact for cats from repository', () async {
+    // arrange
+    // when `getCatFact()` is called with any return the above test object.
+    when(repository.getCatFact()).thenAnswer((_) async => Right(testCatTrivia));
+    // act
+    final result =
+        await useCase(UseCaseParameters(factLimit: 10, maxLength: 200));
+    // assert
+    expect(result, Right(testCatTrivia));
+    verify(repository.getCatFact());
+    verifyNoMoreInteractions(repository);
+  });
 }
